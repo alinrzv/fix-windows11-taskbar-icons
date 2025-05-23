aa550dd2023534d0309c00e1498d1d02ed09d1fd
alinrzv <admin@webake.ro>
2025-05-23 01:04:55 -0700

    Update DeleteTaskband.bat
    Made it so it deletes the cached icons as well .

--- DeleteTaskband.bat
+++ DeleteTaskband.bat
@@ -1,4 +1,17 @@
 @echo off
+echo Deleting Taskband registry key...
 REG DELETE "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband" /f
 echo Taskband key deleted.
+
+echo Deleting taskbar cached icons...
+:: Close Explorer first to release the cache files
+taskkill /f /im explorer.exe
+
+:: Delete IconCache files (taskbar icons are cached here)
+del /f /q "%localappdata%\Microsoft\Windows\Explorer\iconcache*"
+
+:: Restart Explorer
+start explorer.exe
+
+echo Done.
 pause
